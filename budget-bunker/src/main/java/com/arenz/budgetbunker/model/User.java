f5adceba6b3cec99b1d42144a3638a56b32975cc
Arenz Papas <132368840+rnzpapas@users.noreply.github.com>
2025-06-13 16:55:24 +0800

    Explore model annotations.

--- budget-bunker/src/main/java/com/arenz/budgetbunker/model/User.java
+++ budget-bunker/src/main/java/com/arenz/budgetbunker/model/User.java
@@ -1,6 +1,38 @@
 package com.arenz.budgetbunker.model;
 
+import java.util.Date;
 
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import jakarta.persistence.Temporal;
+import jakarta.persistence.TemporalType;
+import jakarta.persistence.GenerationType;
+
+@Entity
+@Table(name = "users")
 public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.SEQUENCE)
+    private Long uid;
     
+    @Column(name = "firstname", nullable = false, length = 50)
+    private String firstname;
+
+    @Column(name = "middlename", nullable = false, length = 50)
+    private String middlename;
+
+    @Column(name = "surname", nullable = false, length = 50)
+    private String surname;
+
+    @Column(name = "username", nullable = false, length = 50, unique = true)
+    private String username;
+
+    @Column(name = "password", nullable = false, length = 50, unique = true)
+    private String password;
+
+    @Temporal(TemporalType.TIMESTAMP)
+    private Date createdAt;
 }
