6fd094b57d7aa14b7596dcd470605caa42a61a80
Arenz Papas <132368840+rnzpapas@users.noreply.github.com>
2025-06-13 17:22:43 +0800

    Explore service.

---
+++ budget-bunker/src/main/java/com/arenz/budgetbunker/service/UserService.java
@@ -0,0 +1,35 @@
+package com.arenz.budgetbunker.service;
+
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.stereotype.Service;
+
+import com.arenz.budgetbunker.model.User;
+import com.arenz.budgetbunker.repository.UserRepository;
+
+@Service
+public class UserService {
+    
+    private final UserRepository userRepository;
+
+    public UserService(UserRepository userRepository) {
+        this.userRepository = userRepository;
+    }
+    
+    public User saveUser(User user) {
+        return userRepository.save(user);
+    }
+
+    public List<User> getAllUsers() {
+        return userRepository.findAll();
+    }
+
+    public Optional<User> getUserById(Long id) {
+        return userRepository.findById(id);
+    }
+
+    public void deleteUserById(Long id) {
+        userRepository.deleteById(id);
+    }
+}
